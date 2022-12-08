0d4f887db60717d7a23080e8f8991996043d307d
oleg.elifantiev <oleg@elifantiev.ru>
2022-12-08 17:18:36 +0300

    DigitMatrix helper

---
+++ digitMatrix.go
@@ -0,0 +1,18 @@
+package goaocd
+
+func DigitMatrix(args ...int) *[][]int {
+	lines := Lines(args...)
+	width := len(lines[0])
+	height := len(lines)
+	mat := make([][]int, height)
+	for i := 0; i < height; i++ {
+		mat[i] = make([]int, width)
+	}
+	for y, line := range lines {
+		for x, s := range line {
+			i := int(s) - '0'
+			mat[y][x] = i
+		}
+	}
+	return &mat
+}
