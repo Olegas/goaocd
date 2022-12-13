5626b5cf4b5ef4180cf8d2c7d439fe235dc93bca
oleg.elifantiev <oleg@elifantiev.ru>
2022-12-13 21:50:53 +0300

    Detect day from program name

--- time.go
+++ time.go
@@ -1,6 +1,11 @@
 package goaocd
 
-import "time"
+import (
+	"os"
+	"path/filepath"
+	"strconv"
+	"time"
+)
 
 func puzzleDate(args ...int) (int, int) {
 	t := time.Now()
@@ -9,6 +14,13 @@ func puzzleDate(args ...int) (int, int) {
 	} else if len(args) == 1 {
 		return t.Year(), args[0]
 	} else {
+		command := filepath.Base(os.Args[0])
+		if command[:3] == "day" {
+			d, err := strconv.Atoi(command[3:])
+			if err == nil {
+				return t.Year(), d
+			}
+		}
 		return t.Year(), t.Day()
 	}
 }
